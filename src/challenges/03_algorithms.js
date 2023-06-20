c35cf7f0c74fe255f85cef088e3c7f5e2ce08c42
Lupe <lupemaydana@gmail.com>
2023-06-20 19:07:05 -0400

    feat: add questions for primeros auxilios y mecanica

--- src/challenges/03_algorithms.js
+++ src/challenges/03_algorithms.js
@@ -33,24 +33,108 @@ export default {
   category: `Primeros Auxilios`,
   challenges: [
     {
-      title: `Las maniobras prohibidas en la conduccion son:`,
-      subtitle: `maniobras prohibidas`,
+      title: `El botiquin de primeros auxilios y el extinguidor de fuego son implementos que solo se lleva cuando se realiza un viaje`,
+      subtitle: `primeros auxilios`,
       choices: [
-        `Ambas alternativas son correctas`,
-        `Girar en "U" en los pasos para los peatornes, en las calles y avenidas`,
-        `En lugares donde la señalización lo prohibe`,
-        `Ninguna de las alternativas es correcta`
+        `No`,
+        `Si`
       ],
       solution: `0`
     },
     {
-      title: ``,
-      subtitle: ``,
+      title: `Como prestaría auxilio a un accidentado grave?`,
+      subtitle: `accidentado grave`,
       choices: [
-        ``,
-        ``
+        `No lo movería del lugar que se encuentra, presto atención y solicito auxilio`,
+        `Lo ayudaría a incorporarse`,
+        `Solamente solicitaría auxilio`
       ],
       solution: `0`
     },
+    {
+      title: `Se debe trasladar a un hospital a una persona gravemente accidentada sin personal de salud capacitado`,
+      subtitle: `trasladar a un herido`,
+      choices: [
+        `No`,
+        `Si`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Cuales son las norma de protección que se deben de tener en cuenta en un accidente de tránsito elija una opcción`,
+      subtitle: `normas de proteccion en accidente`,
+      choices: [
+        `Todas las opcciones son correctas`,
+        `Apartar a las victimas del peligro`,
+        `Señalizar la ruta con las normas vigentes del código de transito y evitar consecuecias más graves`,
+        `Protegerse y proteger a terceras personas`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Según sus conocimientos y criterios cual cree usted que sea una valoración primordial de primeros auxilios que se debe realizar a toda persona victima de un accidente`,
+      subtitle: `primera valoración en accidente`,
+      choices: [
+        `Signos vitales`,
+        `Coloración de la piel`,
+        `Aliento etílico`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Tomando en cuenta que los signos vitales permiten el funcionamiento correcto de nuetro cuerpo humano y forma parte de la valoración primordial ante una victima de accidente de transito, cual de los siguientes enunciados forma parte de los signos vitales`,
+      subtitle: `signos vitales`,
+      choices: [
+        `Respiración`,
+        `Peso`,
+        `Talla`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Si la victima de un accidente presenta la incrustación de un objeto punzo-cortante en cualquier parte de su cuerpo y no presenta hemorragia nunca se debe:`,
+      subtitle: `cuerpo con objeto incrustrado sin hemorragia`,
+      choices: [
+        `Todas las opciones son correctas`,
+        `Abandonar a la victima`,
+        `Remover o retirar el objeto punzo-cortante`,
+        `Todas las opciones son incorrectas`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Si la victima del accidente presenta hemorragia se debe`,
+      subtitle: `hemorragia`,
+      choices: [
+        `Presionar directamente la herida`,
+        `Dar medicamento`,
+        `Abandonar a la victima`
+      ],
+      solution: `0`
+    },
+    {
+      title: `La insolación es un golpe de calor extremo que afecta sobretodo a niños personas obesas y ancianos los mismos presentan malestar general, calambres, mareos, dolor de cabeza, debilidad hasta desmayos, si un pasajero presenta todo lo mencionado anteriormente, cuál será los pasos básicos de primeros auxilios para su atención?`,
+      subtitle: `atencion insolación`,
+      choices: [
+        `Todas las opciones son correctas`,
+        `Colocar a la persona en un lugar fresco y bajo la sombra`,
+        `Aflojar o aligerar ropas`,
+        `Colocar paños húmedos en la cabeza, axilas y cuello`,
+        `Dar de beber agua`,
+        `Todas las opciones son incorrectas`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Las convulciones son ataques de movimientos musculares bruscos e involuntarios con perdidad de conciencia, ante un ataque convulsivo cuales serían las medidas generales y  básicas de primeros auxilios que se deben realizar`,
+      subtitle: `convulciones solucion`,
+      choices: [
+        `Todas las opciones son correctas`,
+        `Proteger a la persona retirando todos los objetos de su alrededor que le puedan causar daño`,
+        `Introducir lateralmente un pañuelo enrollado u objeto duro entre los dientes para evitar que se muerda la lengua`,
+        `Todas las opciones son incorrectas`
+      ],
+      solution: `0`
+    }
   ]
 };
--- src/challenges/04_data-storage.js
+++ src/challenges/04_data-storage.js
@@ -33,24 +33,246 @@ export default {
   category: `Mecánica básica`,
   challenges: [
     {
-      title: `Las maniobras prohibidas en la conduccion son:`,
-      subtitle: `maniobras prohibidas`,
+      title: `La presión de los neumáticos se debe verificar:`,
+      subtitle: `presión de neumaticos`,
       choices: [
-        `Ambas alternativas son correctas`,
-        `Girar en "U" en los pasos para los peatornes, en las calles y avenidas`,
-        `En lugares donde la señalización lo prohibe`,
-        `Ninguna de las alternativas es correcta`
+        `en forma permanente`,
+        `antes de realizar un viaje`,
+        `cuando vea que el neumático está desinflado`
       ],
       solution: `0`
     },
     {
-      title: ``,
-      subtitle: ``,
+      title: `El motor se lubrica con:`,
+      subtitle: `lubricacion motor`,
       choices: [
-        ``,
-        ``
+        `Aceite`,
+        `Agua`,
+        `Gasolina/Diesel`
       ],
       solution: `0`
     },
+    {
+      title: `El mantenimiento de la batería se debe realizar con:`,
+      subtitle: `mantenimiento batería`,
+      choices: [
+        `Limpiando bornes, cables y aumentando agua destilada si falta`,
+        `La ayuda de un especialista`,
+        `Utilizando herramientas especiales`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Que debe revisar periódicamente en el motor?`,
+      subtitle: `revisar motor`,
+      choices: [
+        `Verificar niveles de agua, aceite, líquido de frenos, líquido hidráulico`,
+        `El motor de arranque`,
+        `El alternador`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Antes de poner en marcha un motorizado`,
+      subtitle: `antes de poner en marcha`,
+      choices: [
+        `Debe revisar alrededor del vehículo`,
+        `Debe verificar las luces`,
+        `Debe verificar el combustible`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Que elemento corresponde al sistema eléctrico`,
+      subtitle: `sistema eléctrico`,
+      choices: [
+        `Batería`,
+        `Embriague`,
+        `Agua`,
+        `Combutible`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Si escucha un sonido inapropiado en su vehículo, ¿que acción debe tomar?`,
+      subtitle: `sonido raro vehiculo`,
+      choices: [
+        `localizar el lugar el sonido`,
+        `percibir la magnitud del sonido`,
+        `apagar el motor`
+      ],
+      solution: `0`
+    },
+    {
+      title: `cual de estos elementos no se encuentran en el tablero de control`,
+      subtitle: `tablero de control`,
+      choices: [
+        `las bujías`,
+        `el velocímetro`,
+        `marcador de combustible`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Cual de estos elementos corresponde al sistema de refrigeración`,
+      subtitle: `sistema refrigeración`,
+      choices: [
+        `radiador`,
+        `embrague`,
+        `batería`
+      ],
+      solution: `0`
+    },
+    {
+      title: `que elemento corresponde al sistema eléctrico`,
+      subtitle: `sistema electrico`,
+      choices: [
+        `alternador`,
+        `radiador`,
+        `embrague`,
+        `freno`
+      ],
+      solution: `0`
+    },
+    {
+      title: `que elemento corresponde al sistema eléctrico`,
+      subtitle: `sitema electrico`,
+      choices: [
+        `motor de arranque`,
+        `freno`,
+        `radiador`,
+        `ninguno`
+      ],
+      solution: `0`
+    },
+    {
+      title: `que elemento del automóvil suministra la chispa eléctrica para el encendido de un motor a gasolina o gnv`,
+      subtitle: `encendido de motor chispa`,
+      choices: [
+        `bujías`,
+        `carburador`,
+        `inyector`,
+        `ninguno`
+      ],
+      solution: `0`
+    },
+    {
+      title: `un motor a diésel necesita la chispa generada por la bujía para su encendido`,
+      subtitle: `motor diésel chispa`,
+      choices: [
+        `No`,
+        `Si`
+      ],
+      solution: `0`
+    },
+    {
+      title: `El embrague es un elemento fundamental del sistema de transmición en un auto con caja mecánica`,
+      subtitle: `embrague sistema de transmición`,
+      choices: [
+        `Si`,
+        `No`
+      ],
+      solution: `0`
+    },
+    {
+      title: `El sistema de freno se utiliza para`,
+      subtitle: `sistema de freno`,
+      choices: [
+        `Detener o disminuir la marcha del vehículo`,
+        `Realizar un cambio de velocidad`,
+        `Para arrancar el vehículo`
+      ],
+      solution: `0`
+    },
+    {
+      title: `El freno de mano a que ruedas acciona`,
+      subtitle: `freno de mano`,
+      choices: [
+        `ruedas traseras`,
+        `ruedas delanteras`,
+        `las cuatro ruedas`
+      ],
+      solution: `0`
+    },
+    {
+      title: `el liquido refrigerante (agua) sirve para`,
+      subtitle: `liquido refrigerante`,
+      choices: [
+        `enfriar el motor`,
+        `enfriar los frenos de las ruedas`,
+        `enfriar la cada de cambios`
+      ],
+      solution: `0`
+    },
+    {
+      title: `la batería de un vehículo liviano tiene`,
+      subtitle: `bateria de vehiculo`,
+      choices: [
+        `12 voltios`,
+        `9 voltios`,
+        `15 voltios`,
+        `24 voltios`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Los muñones o brazos de articulación pertenecen a:`,
+      subtitle: `muñones`,
+      choices: [
+        `Sistema de dirección`,
+        `Sistema de calefacción`,
+        `Sistema de suspención`
+      ],
+      solution: `0`
+    },
+    {
+      title: `que elemento pertenecen al sistema de frenos`,
+      subtitle: `sistema de frenos`,
+      choices: [
+        `pastillas de freno y balatas`,
+        `el radiador`,
+        `la batería`
+      ],
+      solution: `0`
+    },
+    {
+      title: `que elementos pertenecen al sistema de suspención`,
+      subtitle: `sistema suspencion`,
+      choices: [
+        `los amortiguadores`,
+        `el alternador`,
+        `los frenos`
+      ],
+      solution: `0`
+    },
+    {
+      title: `la dirección de un nuevo vehículo que no es mecánica con que líquido trabaja`,
+      subtitle: `direccion no mecanica`,
+      choices: [
+        `líquido o aceite hidráulico`,
+        `líquido de frenos`,
+        `agua o anticongelante`
+      ],
+      solution: `0`
+    },
+    {
+      title: `en un vehículo accidentado deberá desconectar los bornes de la batería`,
+      subtitle: `bornes`,
+      choices: [
+        `si`,
+        `no`
+      ],
+      solution: `0`
+    },
+    {
+      title: `si su batería no esta a su nivel debido que líquido se debe aumentar:`,
+      subtitle: `bateria a desnivel`,
+      choices: [
+        `Agua destilada`,
+        `Anticongelante`,
+        `Aceite`
+      ],
+      solution: `0`
+    }
   ]
 };
