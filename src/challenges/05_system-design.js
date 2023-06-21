1b77a4c67f4593d80fcdf49fbb1d5560167ec16d
Lupe <lupemaydana@gmail.com>
2023-06-21 13:17:14 -0400

    feat: add questions for Legislación Boliviana section

--- src/challenges/05_system-design.js
+++ src/challenges/05_system-design.js
@@ -33,24 +33,272 @@ export default {
   category: `Legislación Boliviana`,
   challenges: [
     {
-      title: `Las maniobras prohibidas en la conduccion son:`,
-      subtitle: `maniobras prohibidas`,
+      title: `En ciudades y poblaciones los peatones circularan por las aceras conservando su derecha evitando en lo posible el uso de la clazada.`,
+      subtitle: `uso de la calzada`,
       choices: [
-        `Ambas alternativas son correctas`,
-        `Girar en "U" en los pasos para los peatornes, en las calles y avenidas`,
-        `En lugares donde la señalización lo prohibe`,
-        `Ninguna de las alternativas es correcta`
+        `Verdadero`,
+        `Falso`
       ],
       solution: `0`
     },
     {
-      title: ``,
-      subtitle: ``,
+      title: `Las motocicletas, bicicletas y otros veh[iculos menores circularan por las vías públicas obligatoriamente en columna`,
+      subtitle: `circulacion via publica`,
       choices: [
-        ``,
-        ``
+        `Verdadero`,
+        `Falso`
       ],
       solution: `0`
     },
+    {
+      title: `Es prohibida la circulación de motocicletas, bicicletas y otro vehículos menores por las aceras`,
+      subtitle: `circulacion prohibida aceras`,
+      choices: [
+        `Verdadero`,
+        `Falso`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Está prohibido realizar actos de acrobacia con motocicletas y otros vehículos menores`,
+      subtitle: `actos de acrobacia`,
+      choices: [
+        `Verdadero`,
+        `Falso`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Por conducir motocicleta sin el respectivo casco de seguridad es una infracción de:`,
+      subtitle: `conducir sin casco`,
+      choices: [
+        `Tercer grado`,
+        `Segundo grado`,
+        `Primer grado`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Por conducir motocicleta con el escape libre es una infracción de:`,
+      subtitle: `conducir con escape libre`,
+      choices: [
+        `Tercer grado`,
+        `Segundo grado`,
+        `Primer grado`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Por confiar o permitir la conducción de su vehículo a persona que no tenga licencia de conducir es una infracción de:`,
+      subtitle: `conducir sin licencia`,
+      choices: [
+        `Primer grado`,
+        `Tercer grado`,
+        `Segundo grado`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Los operadores del transporte público terrestre de pasajeros tienen la obligación de:`,
+      subtitle: `transporte publico obligacion`,
+      choices: [
+        `Acreditar a los conductores ante el viceministerio de transporte`,
+        `Verificar carreteras y caminos`,
+        `Cumplir las señalizaciones`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Se consideran también infracciones:`,
+      subtitle: `infracciones`,
+      choices: [
+        `Todas las alternativas son correctas`,
+        `Incumplir los horarios programados de salida de la terminal`,
+        `Trasnportar equipajes y la carga de los asientos desocupados del bus.`,
+        `Transportar pasajeros en los pasillos, buzones y cabina de bus.`
+      ],
+      solution: `0`
+    },
+    {
+      title: `Usar la bocina en forma indebida en horas del día. ¿qué tipo de infracción es?`,
+      subtitle: `uso de bocina`,
+      choices: [
+        `Tercer Grado`,
+        `Segundo Grado`,
+        `Primer Grado`
+      ],
+      solution: `0`
+    },
+    {
+      "title": "Cuantos grados de infracciones describe el codigo de transito.",
+      "subtitle": "grados, infracciones",
+      "choices": [
+        "tres",
+        "dos",
+        "cuatro"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "SEGÚN EL CÓDIGO DE TRANSITO CONDUCIR UN VEHÍCULO MOTORIZADO EN ESTADO DE EBRIEDAD ES:",
+      "subtitle": "estado, ebriedad",
+      "choices": [
+        "Infracción",
+        "Delito",
+        "Ninguna de las anteriores"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "INFRACCION DE SEGUNDO GRADO ES:",
+      "subtitle": "segundo grado",
+      "choices": [
+        "Viajar sin herramientas",
+        "Conducir en estado de ebriedad",
+        "Por no conservar su derecha al conducir un vehículo"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "POR ESTACIONAR INCORRECTAMENTE EN VIAS URBANAS O RURALES ES UNA INFRACCIÓN:",
+      "subtitle": "estacionar, vias urbanas, vias rurales",
+      "choices": [
+        "Tercer grado",
+        "Primer grado",
+        "Segundo grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "INFRACCIÓN DE PRIMER GRADO ES",
+      "subtitle": "primer grado, placas alteradas, un solo farol, escape libre",
+      "choices": [
+        "usar placas alteradas que no corresponden al vehículo",
+        "Conducir un vehículo con un solo farol",
+        "Circular con el escape libre dentro de la ciudad"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "INFRACCIÓN DE SEGUNDO GRADO ES",
+      "subtitle": "segundo grado, desacato a la autoridad, estado de ebriedad, impedir el tránsito",
+      "choices": [
+        "Desacato a la autoridad por parte conductores, usuarios y peatones",
+        "Conducir un vehículo en estado de ebriedad",
+        "Impedir el tránsito sin causa justificada"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "INFRACCION DE TERCER GRADO ES",
+      "subtitle": "tercer grado, ninguna alternativa, desacato a la autoridad, exhibir licencia, estacionar en lugares prohibidos",
+      "choices": [
+        "Ninguna de las alternativas es correcta",
+        "Desacato a la autoridad por parte de conductores, usuarios y peatones",
+        "Negarse a exhibir la licencia de conducir a la autoridad",
+        "Estacionar en lugares prohibidos o de reservación oficial"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "Desacato a la autoridad por parte de los conductores, usuarios y peatones, ¿qué clase de infracción es?",
+      "subtitle": "desacato a la autoridad, conductores, usuarios, peatones",
+      "choices": [
+        "Segundo grado",
+        "Primer grado",
+        "Tercer grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "¿Qué clase de infracción es conducir sin haber recabado la licencia o autorización respectiva?",
+      "subtitle": "conducir, licencia, autorización",
+      "choices": [
+        "Primer grado",
+        "Segundo grado",
+        "Tercer grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "¿Qué clase de infracción es conducir un vehículo sin?",
+      "subtitle": "conducir, vehículo",
+      "choices": [
+        "Tercer grado",
+        "Primer grado",
+        "Segundo grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "Transitar sin luces es infracción de",
+      "subtitle": "transitar, luces",
+      "choices": [
+        "Primer grado",
+        "Segundo grado",
+        "Tercer grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "Por circular con el vehículo en malas condiciones de funcionamiento es infracción de",
+      "subtitle": "circulcar, vehículo, malas condiciones, funcionamiento",
+      "choices": [
+        "Segundo grado",
+        "Primer grado",
+        "Tercer grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "Circular con exceso de velocidad es infracción de",
+      "subtitle": "circular, exceso de velocidad",
+      "choices": [
+        "Primer grado",
+        "Segundo grado",
+        "Tercer grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "Por circular en contra ruta señalada es una infracción de",
+      "subtitle": "circulcar, contra ruta señalada",
+      "choices": [
+        "Primer grado",
+        "Segundo grado",
+        "Tercer grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "LA FUGA Y LA FALTA DE ASISTENCIA EN UN ACCIDENTE DE TRÁNSITO ES UNA FALTA DE:",
+      "subtitle": "fuga, falta de asistencia, accidente de tránsito",
+      "choices": [
+        "Primer grado",
+        "Segundo grado",
+        "Tercer grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "POR ESTACIONAR EN LUGARES PROHIBIDOS O DE RESERVACIÓN OFICIAL ES INFRACCIÓN DE",
+      "subtitle": "estacionar, lugares prohibidos, reservación oficial",
+      "choices": [
+        "Primer grado",
+        "Segundo grado",
+        "Tercer grado"
+      ],
+      "solution": "0"
+    },
+    {
+      "title": "EL QUE RESULTARE CULPABLE DE LA MUERTE O Produjera LESIONES GRAVES O Gravísimas DE UNA O MAS PERSONAS Ocasionado POR UN MEDIO DE TRANSPORTE MOTORIZADO Y EL CONDUCTOR SE ENCONTRARE BAJO EFECTOS DE ALCOHOL O ESTUPEFACIENTE. QUE COMETE?",
+      "subtitle": "muerte, lesiones graves, gravísimas, medio de transporte, conductor, efectos, alcohol, estupefaciente",
+      "choices": [
+        "Infracción",
+        "Delito",
+        "Ninguna es correcta"
+      ],
+      "solution": "0"
+    }
   ]
 };
